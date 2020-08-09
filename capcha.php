86d76b1c1236a52eb49ecc46aa958b6f1d3d0fbe
Julibe <mail@julibe.com>
2020-08-09 17:42:50 -0500

    Upload of files

---
+++ capcha.php
@@ -0,0 +1,98 @@
+<?php
+	/* Generar imagen CAPCHA */
+
+	/* Star Sesion */
+	session_start();
+
+	/* Characters to use */
+	$permitted_chars = 'abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+	/*
+		Generate random string
+		$input = Characters to use
+		$strength = number of characters
+	*/
+	function generate_string($input, $strength = 5) {
+		$input_length = strlen($input);
+		$random_string = '';
+		for($i = 0; $i < $strength; $i++) {
+			$random_character = $input[mt_rand(0, $input_length - 1)];
+			$random_string .= $random_character;
+		}	
+		return $random_string;
+	}
+	
+	/*  */
+
+	ob_start();
+		$image = imagecreatetruecolor(200, 50);
+		imageantialias($image, true);
+
+		$colors = [];
+
+		$red = rand(125, 175);
+		$green = rand(125, 175);
+		$blue = rand(125, 175);
+
+		$red = 234;
+		$green = 28;
+		$blue = 38;
+
+		for($i = 0; $i < 5; $i++) {
+			$colors[] = imagecolorallocate($image, $red - 20*$i, $green - 5*$i, $blue - 7*$i);
+		}
+
+		imagefill($image, 0, 0, $colors[0]);
+
+		for($i = 0; $i < 10; $i++) {
+			imagesetthickness($image, rand(2, 10));
+			$rect_color = $colors[rand(1, 4)];
+			imagerectangle($image, rand(-10, 190), rand(-10, 10), rand(-10, 190), rand(40, 60), $rect_color);
+		}
+
+		$black = imagecolorallocate($image, 0, 0, 0);
+		$white = imagecolorallocate($image, 255, 255, 255);
+		$textcolors = [$black, $white];
+		
+
+		$font_url = getcwd().trim('/./fonts/ ');
+
+		$font_name='noah-regular.ttf';
+		$font= $font_url.$font_name;
+
+		$font_name_2='roboto-regular.ttf';
+		$font_2= $font_url.$font_name_2;
+
+		$font_name_3='noah-bold.ttf';
+		$font_3= $font_url.$font_name_3;
+
+		$font_name_4='lulocleanone-bold.ttf';
+		$font_4= $font_url.$font_name_4;		
+
+		$string_length = 6;
+		$captcha_string = generate_string($permitted_chars, $string_length);
+
+		if(is_file($font)){
+
+			$fonts = [$font, $font_4, $font_3, $font_4];
+
+
+			for($i = 0; $i < $string_length; $i++) {
+				$letter_space = 170/$string_length;
+				$initial = 15;
+			
+				imagettftext($image, 20, rand(-15, 15), $initial + $i*$letter_space, rand(20, 40), $textcolors[rand(0, 1)], $fonts[array_rand($fonts)], $captcha_string[$i]);
+			}
+			
+		}
+		imagepng($image);
+		imagedestroy($image);
+
+	$buffer = ob_get_clean();
+	//$base64 = 'data:image/png;base64,' . base64_encode($buffer);
+	header('Content-type: image/png');
+	$_SESSION['captcha_text'] = $captcha_string;
+	//e($base64 );
+	echo( $buffer );
+	//echo( $captcha_string );
+?>
