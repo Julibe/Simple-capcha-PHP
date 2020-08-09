f83c7e52fcc2e4154fd8062009626b1d77fc6955
Julibe <mail@julibe.com>
2020-08-09 18:32:14 -0500

    Updates of comments

--- capcha.php
+++ capcha.php
@@ -22,41 +22,51 @@
 		return $random_string;
 	}
 	
-	/*  */
+	/* opens a buffer in which all output is stored */
 
 	ob_start();
+		/* Creates an image */
+
 		$image = imagecreatetruecolor(200, 50);
 		imageantialias($image, true);
 
 		$colors = [];
 
-		$red = rand(125, 175);
-		$green = rand(125, 175);
-		$blue = rand(125, 175);
+		/* Generate a random color */
 
-		$red = 234;
-		$green = 28;
-		$blue = 38;
+		$v_c = 25;//variable color
+		$red = rand($v_c , 255 - $v_c );
+		$green =  rand($v_c , 255 - $v_c );
+		$blue =  rand($v_c , 255 - $v_c );
 
-		for($i = 0; $i < 5; $i++) {
-			$colors[] = imagecolorallocate($image, $red - 20*$i, $green - 5*$i, $blue - 7*$i);
+
+		/* Generate variations to the color */
+		for($i = 0; $i < $v_c; $i++) {
+			$r = rand(-$v_c ,$v_c );
+			$g = rand(-$v_c ,$v_c );
+			$b =rand(-$v_c ,$v_c );
+			$colors[] = imagecolorallocate($image, $red+$r , $green+$g, $blue+$b);
 		}
 
+		/* Add the background to the image */
 		imagefill($image, 0, 0, $colors[0]);
 
+		/* Add random bars of collor to the image */
 		for($i = 0; $i < 10; $i++) {
 			imagesetthickness($image, rand(2, 10));
 			$rect_color = $colors[rand(1, 4)];
 			imagerectangle($image, rand(-10, 190), rand(-10, 10), rand(-10, 190), rand(40, 60), $rect_color);
 		}
 
+		/* Colors for the text */
 		$black = imagecolorallocate($image, 0, 0, 0);
 		$white = imagecolorallocate($image, 255, 255, 255);
 		$textcolors = [$black, $white];
 		
-
+		/* Get the folder of the images */
 		$font_url = getcwd().trim('/./fonts/ ');
-
+		
+		/* add the name og the fonts */
 		$font_name='noah-regular.ttf';
 		$font= $font_url.$font_name;
 
@@ -69,6 +79,7 @@
 		$font_name_4='lulocleanone-bold.ttf';
 		$font_4= $font_url.$font_name_4;		
 
+		/* $string_length = number of characters six for this example */
 		$string_length = 6;
 		$captcha_string = generate_string($permitted_chars, $string_length);
 
@@ -85,14 +96,20 @@
 			}
 			
 		}
+		/* Create the image and delete it */
 		imagepng($image);
 		imagedestroy($image);
 
+		
+	/* Close the buffer */
+
 	$buffer = ob_get_clean();
-	//$base64 = 'data:image/png;base64,' . base64_encode($buffer);
+	
+	/* Show the code as an image */
 	header('Content-type: image/png');
+
+	/* Add the string to the session */
+
 	$_SESSION['captcha_text'] = $captcha_string;
-	//e($base64 );
 	echo( $buffer );
-	//echo( $captcha_string );
 ?>
